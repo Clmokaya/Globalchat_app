9fdc4c165e4c6d7823adcafe89d1f69529320b93
clmokaya <mokmakori@gmail.com>
2025-02-12 10:09:59 +0300

    Enabling firebase authentication,using forms

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -8,11 +9,16 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  var user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard'),
+      ),
+      body: Column(
+        children: [Text('Welcome,'), Text((user?.email ?? "").toString())],
       ),
     );
   }
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:globalchat/screens/dashboard_screen.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  var userForm = GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  Future<void> createAccount() async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email.text, password: password.text);
+
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute(builder: (context) {
+        return DashboardScreen();
+      }), (route) {
+        return false;
+      });
+
+      print('Account created successfully!');
+    } catch (e) {
+      SnackBar messageSnackBar =
+          SnackBar(backgroundColor: Colors.red, content: Text(e.toString()));
+
+      ScaffoldMessenger.of(context).showSnackBar(messageSnackBar);
+
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Signup'),
+      ),
+      body: Form(
+        key: userForm,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: email,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Email is required";
+                  }
+                },
+                decoration: InputDecoration(label: Text('Email')),
+              ),
+              SizedBox(height: 23),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: password,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Password is required";
+                  }
+                },
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(label: Text('Password')),
+              ),
+              SizedBox(height: 23),
+              ElevatedButton(
+                  onPressed: () {
+                    if (userForm.currentState!.validate()) {
+                      createAccount(); //create account
+                    }
+                  },
+                  child: Text('Create account'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
