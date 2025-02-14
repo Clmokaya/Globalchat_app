5243b858462b963cda84a4c9abf6e569947ef05a
clmokaya <mokmakori@gmail.com>
2025-02-14 21:27:03 +0300

    working on the dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:globalchat/providers/userProvider.dart';
 import 'package:globalchat/screens/profile_screen.dart';
 import 'package:globalchat/screens/splash_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -12,56 +15,107 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   var user = FirebaseAuth.instance.currentUser;
+  var db = FirebaseFirestore.instance;
+
+  List<Map<String, dynamic>> chatroomsList = [];
+
+  void getChatrooms() {
+    db.collection("chatrooms").get().then((dataSnapshot) {
+      for (var singleChatroomData in dataSnapshot.docs) {
+        chatroomsList.add(singleChatroomData.data());
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    getChatrooms(); // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    var userProvider = Provider.of<UserProvider>(context);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Global Chat')),
-      drawer: Drawer(
-        child: Container(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              ListTile(
-                onTap: () async {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return ProfileScreen();
-                    }),
-                  );
-                },
-                leading: Icon(Icons.people),
-                title: Text('Profile'),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              ListTile(
-                onTap: () async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.pushAndRemoveUntil(context,
-                      MaterialPageRoute(builder: (context) {
-                    return SplashScreen();
-                  }), (route) {
-                    return false;
-                  });
-                },
-                leading: Icon(Icons.logout),
-                title: Text('Logout'),
-              )
-            ],
+        appBar: AppBar(
+          title: Text('Global Chat'),
+          leading: CircleAvatar(
+            child: Text(userProvider.userName[0]),
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          Text('Welcome,'),
-          Text((user?.email ?? "").toString()),
-        ],
-      ),
-    );
+        drawer: Drawer(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                ListTile(
+                  onTap: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return ProfileScreen();
+                      }),
+                    );
+                  },
+                  leading: CircleAvatar(
+                    child: Text(userProvider.userName[0]),
+                  ),
+                  title: Text(
+                    userProvider.userName,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(userProvider.userEmail),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                ListTile(
+                  onTap: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return ProfileScreen();
+                      }),
+                    );
+                  },
+                  leading: Icon(Icons.people),
+                  title: Text('Profile'),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                ListTile(
+                  onTap: () async {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.pushAndRemoveUntil(context,
+                        MaterialPageRoute(builder: (context) {
+                      return SplashScreen();
+                    }), (route) {
+                      return false;
+                    });
+                  },
+                  leading: Icon(Icons.logout),
+                  title: Text('Logout'),
+                )
+              ],
+            ),
+          ),
+        ),
+        body: ListView.builder(
+            itemCount: chatroomsList.length,
+            itemBuilder: (BuildContext context, int index) {
+              String chatroomName = chatroomsList[index]["chatroom_name"] ?? "";
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Text(chatroomName[0]),
+                ),
+                title: Text(chatroomName),
+                subtitle: Text(chatroomsList[index]["desc"] ?? ""),
+              );
+            }));
   }
 }
