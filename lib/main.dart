c7c61d217ed597738db4eba0b61821b44b6d4456
clmokaya <mokmakori@gmail.com>
2025-02-20 18:48:32 +0300

    debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class _myAppState extends State<myApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           brightness: Brightness.light,
           useMaterial3: true,
